43b70e7dd9b2b64f5e700a735bbf4538ff6f983b
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-01-24 13:26:23 +0200

    change apps.py

--- src/cities/apps.py
+++ src/cities/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class CitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cities'
-    verbose_name = 'Города'
+    verbose_name = 'Населенные пункты'
 
