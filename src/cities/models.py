2d9565b457ccb432b9d05b9ec0a2da0914545971
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-01-24 13:26:03 +0200

    change model City

--- src/cities/models.py
+++ src/cities/models.py
@@ -5,6 +5,9 @@ class City(models.Model):
     name = models.CharField(max_length=100, help_text='Не боллее 100 символов',
                             unique=True, verbose_name='Название города')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
